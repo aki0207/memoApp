3e2720079a014c41047f136f02736b56ceb83812
aki <forever.look.aki27@gmail.com>
2019-11-21 21:39:23 +0900

    とりあえずテーブルから取得はできた

---
+++ src/main/java/com/demo/bean/ArticleBean.java
@@ -0,0 +1,44 @@
+package com.demo.bean;
+
+public class ArticleBean {
+	
+	int id;
+	String title;
+	String text;
+	String postDate;
+	String lastUpDate;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getPostDate() {
+		return postDate;
+	}
+	public void setPostDate(String postDate) {
+		this.postDate = postDate;
+	}
+	public String getLastUpDate() {
+		return lastUpDate;
+	}
+	public void setLastUpDate(String lastUpDate) {
+		this.lastUpDate = lastUpDate;
+	}
+	
+
+}
